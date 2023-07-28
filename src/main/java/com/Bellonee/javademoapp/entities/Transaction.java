f3787db1651a6f3f1aae6ee5b9a9f5c763fa6d2b
Wajiu Olarewaju Bello <129260539+Bellonee29@users.noreply.github.com>
2023-07-28 18:52:35 +0100

    Bank application demo

---
+++ src/main/java/com/Bellonee/javademoapp/entities/Transaction.java
@@ -0,0 +1,27 @@
+package com.Bellonee.javademoapp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private String transactionId;
+
+    private String transactionType;
+    private BigDecimal amount;
+    private String accountNumber;
+    private String status;
+}
---
+++ src/main/java/com/Bellonee/javademoapp/services/EmailService.java
@@ -0,0 +1,7 @@
+package com.Bellonee.javademoapp.services;
+
+import com.Bellonee.javademoapp.dto.EmailDetails;
+
+public interface EmailService {
+    void sendEmailAlart(EmailDetails emailDetails);
+}
